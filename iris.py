e2bc559da4693ef49332f2ec257dae7b1c3ac564
Pantagon <pichayoothunchainao@gmail.com>
2024-02-06 01:39:01 +0700

    iris

--- iris.py
+++ iris.py
@@ -7,240 +7,202 @@ from Dtreefunc import *
 f = open("preprocessed_iris.txt", "r")
 X = f.readlines()
 
-# เตรียม พื้นที่เพื่อเก็บผลการคำนวณแยกตาม class
-N = 3  # col(3 class)
-M = 3  # row
 
-sl = np.zeros(3)
-slCI = [[0 for i in range(M)] for j in range(N)]
+# Initialize counters and matrices
+N = 3  # Number of classes
+M = 4  # Number of attributes
 
-sw = np.zeros(3)
-swCI = [[0 for i in range(M)] for j in range(N)]
+sl = np.zeros(N)
+slCI = np.zeros((N, M+1))  # M+1 for the entropy column
 
-pl = np.zeros(3)
-plCI = [[0 for i in range(M)] for j in range(N)]
+sw = np.zeros(N)
+swCI = np.zeros((N, M+1))
 
-pw = np.zeros(3)
-pwCI = [[0 for i in range(M)] for j in range(N)]
+pl = np.zeros(N)
+plCI = np.zeros((N, M+1))
 
-classs = np.zeros(3)
+pw = np.zeros(N)
+pwCI = np.zeros((N, M+1))
 
-# วน loop เพื่อนับข้อมูล แยกตามรายละเอียด attb และ class
-for i in range(0, 15):
+classs = np.zeros(N)
+
+# Loop through the data to count occurrences
+for i in range(1, 151):
+    # Sepal Length (sl)
+    print(f"Processing line {i}: {X[i]}")
     if X[i].count("S") == 1:
-        sl[0] += 1  
-        if ((X[i].count("S") == 1)) and (X[i].count("Iris-setosa") == 1):
-            slCI[0][0] += 1  
-        elif ((X[i].count("S") == 1)) and (X[i].count("Iris-versicolor") == 1):
-            slCI[0][1] += 1
-        elif ((X[i].count("S") == 1)) and (X[i].count("IIris-virginica") == 1):
-            slCI[0][2] += 1
+        sl[0] += 1
+        slCI[0][0] += 1 if "Iris-setosa" in X[i] else 0
+        slCI[1][0] += 1 if "Iris-versicolor" in X[i] else 0
+        slCI[2][0] += 1 if "Iris-virginica" in X[i] else 0
     elif X[i].count("M") == 1:
         sl[1] += 1
-        if ((X[i].count("M") == 1)) and (X[i].count("Iris-setosa") == 1):
-            slCI[0][0] += 1  
-        elif ((X[i].count("M") == 1)) and (X[i].count("Iris-versicolor") == 1):
-            slCI[0][1] += 1
-        elif ((X[i].count("M") == 1)) and (X[i].count("IIris-virginica") == 1):
-            slCI[0][2] += 1
+        slCI[0][1] += 1 if "Iris-setosa" in X[i] else 0
+        slCI[1][1] += 1 if "Iris-versicolor" in X[i] else 0
+        slCI[2][1] += 1 if "Iris-virginica" in X[i] else 0
     elif X[i].count("L") == 1:
         sl[2] += 1
-        if ((X[i].count("L") == 1)) and (X[i].count("Iris-setosa") == 1):
-            slCI[0][0] += 1  
-        elif ((X[i].count("L") == 1)) and (X[i].count("Iris-versicolor") == 1):
-            slCI[0][1] += 1
-        elif ((X[i].count("L") == 1)) and (X[i].count("IIris-virginica") == 1):
-            slCI[0][2] += 1
+        slCI[0][2] += 1 if "Iris-setosa" in X[i] else 0
+        slCI[1][2] += 1 if "Iris-versicolor" in X[i] else 0
+        slCI[2][2] += 1 if "Iris-virginica" in X[i] else 0
 
-    if X[i].count("low") == 1:
-        pl[0] += 1
-        if ((X[i].count("S") == 1)) and (X[i].count("No") == 1):
-            plCI[0][0] += 1  # class no
-        else:
-            plCI[0][1] += 1  # class yes
-    elif X[i].count("medium") == 1:
-        pl[1] += 1
-        if ((X[i].count("medium") == 1)) and (X[i].count("No") == 1):
-            plCI[1][0] += 1
-        else:
-            plCI[1][1] += 1
-    elif X[i].count("high") == 1:
-        pl[2] += 1
-        if ((X[i].count("high") == 1)) and (X[i].count("No") == 1):
-            plCI[2][0] += 1
-        else:
-            plCI[2][1] += 1
-
-    if X[i].count("s_no") == 1:
+    # Sepal Width (sw)
+    if X[i].count("S") == 1:
         sw[0] += 1
-        if ((X[i].count("s_no") == 1)) and (X[i].count("No") == 1):
-            swCI[0][0] += 1  # class no
-        else:
-            swCI[0][1] += 1  # class yes
-    elif X[i].count("s_yes") == 1:
+        swCI[0][0] += 1 if "Iris-setosa" in X[i] else 0
+        swCI[1][0] += 1 if "Iris-versicolor" in X[i] else 0
+        swCI[2][0] += 1 if "Iris-virginica" in X[i] else 0
+    elif X[i].count("M") == 1:
         sw[1] += 1
-        if ((X[i].count("s_yes") == 1)) and (X[i].count("No") == 1):
-            swCI[1][0] += 1
-        else:
-            swCI[1][1] += 1
+        swCI[0][1] += 1 if "Iris-setosa" in X[i] else 0
+        swCI[1][1] += 1 if "Iris-versicolor" in X[i] else 0
+        swCI[2][1] += 1 if "Iris-virginica" in X[i] else 0
+    elif X[i].count("L") == 1:
+        sw[2] += 1
+        swCI[0][2] += 1 if "Iris-setosa" in X[i] else 0
+        swCI[1][2] += 1 if "Iris-versicolor" in X[i] else 0
+        swCI[2][2] += 1 if "Iris-virginica" in X[i] else 0
 
-    if X[i].count("fair") == 1:
+    # Petal Length (pl)
+    if X[i].count("S") == 1:
+        pl[0] += 1
+        plCI[0][0] += 1 if "Iris-setosa" in X[i] else 0
+        plCI[1][0] += 1 if "Iris-versicolor" in X[i] else 0
+        plCI[2][0] += 1 if "Iris-virginica" in X[i] else 0
+    elif X[i].count("M") == 1:
+        pl[1] += 1
+        plCI[0][1] += 1 if "Iris-setosa" in X[i] else 0
+        plCI[1][1] += 1 if "Iris-versicolor" in X[i] else 0
+        plCI[2][1] += 1 if "Iris-virginica" in X[i] else 0
+    elif X[i].count("L") == 1:
+        pl[2] += 1
+        plCI[0][2] += 1 if "Iris-setosa" in X[i] else 0
+        plCI[1][2] += 1 if "Iris-versicolor" in X[i] else 0
+        plCI[2][2] += 1 if "Iris-virginica" in X[i] else 0
+
+    # Petal Width (pw)
+    if X[i].count("S") == 1:
         pw[0] += 1
-        if ((X[i].count("fair") == 1)) and (X[i].count("No") == 1):
-            pwCI[0][0] += 1  # class no
-        else:
-            pwCI[0][1] += 1  # class yes
-    elif X[i].count("excellent") == 1:
+        pwCI[0][0] += 1 if "Iris-setosa" in X[i] else 0
+        pwCI[1][0] += 1 if "Iris-versicolor" in X[i] else 0
+        pwCI[2][0] += 1 if "Iris-virginica" in X[i] else 0
+    elif X[i].count("M") == 1:
         pw[1] += 1
-        if ((X[i].count("excellent") == 1)) and (X[i].count("No") == 1):
-            pwCI[1][0] += 1
-        else:
-            pwCI[1][1] += 1
+        pwCI[0][1] += 1 if "Iris-setosa" in X[i] else 0
+        pwCI[1][1] += 1 if "Iris-versicolor" in X[i] else 0
+        pwCI[2][1] += 1 if "Iris-virginica" in X[i] else 0
+    elif X[i].count("L") == 1:
+        pw[2] += 1
+        pwCI[0][2] += 1 if "Iris-setosa" in X[i] else 0
+        pwCI[1][2] += 1 if "Iris-versicolor" in X[i] else 0
+        pwCI[2][2] += 1 if "Iris-virginica" in X[i] else 0
 
-    if X[i].count("Iris-setosa") == 1:
+    # Class
+    if "Iris-setosa" in X[i]:
         classs[0] += 1
-    elif X[i].count("Iris-versicolor") == 1:
+    elif "Iris-versicolor" in X[i]:
         classs[1] += 1
-    elif X[i].count("Iris-virginica") == 1:
+    elif "Iris-virginica" in X[i]:
         classs[2] += 1
 
 
+
 # calculate information gain of dataset and attb
 # info D,sl,pl,sw,pw
-    info_d = calculate_entropy(classs)
+info_d = calculate_entropy(classs[0], classs[1], classs[2])
+print("Iris-setosa",classs[0], "  Iris-versicolor",classs[1],"  Iris-virginica", classs[2])
+print("Entropy of 1st Dataset = " , info_d)
 
-    for i in range(3):
-        slCI[i][3] = calculate_entropy(
-            [slCI[i][0], slCI[i][1], slCI[i][2]])
-        swCI[i][3] = calculate_entropy(
-            [swCI[i][0], swCI[i][1], swCI[i][2]])
-        plCI[i][3] = calculate_entropy(
-            [plCI[i][0], plCI[i][1], plCI[i][2]])
-        pwCI[i][3] = calculate_entropy(
-            [pwCI[i][0], pwCI[i][1], pwCI[i][2]])
+print()
+print(slCI[0][0],slCI[0][1],slCI[0][2])
+print(slCI[1][0],slCI[1][1],slCI[1][2])
+print(slCI[2][0],slCI[2][1],slCI[2][2])
+print()
+print(swCI[0][0],swCI[0][1],swCI[0][2])
+print(swCI[1][0],swCI[1][1],swCI[1][2])
+print(swCI[2][0],swCI[2][1],swCI[2][2])
+print()
+print(plCI[0][0],plCI[0][1],plCI[0][2])
+print(plCI[1][0],plCI[1][1],plCI[1][2])
+print(plCI[2][0],plCI[2][1],plCI[2][2])
+print()
 
-# หาค่า gain แบบไม่ใช้ และใช้ฟังก์ชัน
-"""
-การหาแบบไม่ใช้ฟังก์ชัน
-Info_slD = ((sl[0]/14)*slCI[0][2])+((sl[1]/14)*slCI[1][2])+((sl[2]/14)*slCI[2][2])
-print("InfoD sl is",Info_slD)
-print("sl Ci [:],[2] is",[slCI[0][2],slCI[1][2],slCI[2][2]])
-print("InfoD sl is",Info_slD)
-"""
-Info_slD = inforD(sl, [slCI[0][2], slCI[1][2], slCI[2][2]])
-Info_plD = inforD(pl, [plCI[0][2], plCI[1][2], plCI[2][2]])
-Info_swD = inforD(sw, [swCI[0][2], swCI[1][2]])
-Info_pwD = inforD(pw, [pwCI[0][2], pwCI[1][2]])
+slCI[0][3] = calculate_entropy(slCI[0][0], slCI[0][1], slCI[0][2])
+slCI[1][3] = calculate_entropy(slCI[1][0], slCI[1][1], slCI[1][2])
+slCI[2][3] = calculate_entropy(slCI[2][0], slCI[2][1], slCI[2][2])
+
+# for i in range(3):
+#     slCI[i][3] = calculate_entropy(
+#         slCI[i][0], slCI[i][1], slCI[i][2])
+#     swCI[i][3] = calculate_entropy(
+#         swCI[i][0], swCI[i][1], swCI[i][2])
+#     plCI[i][3] = calculate_entropy(
+#         plCI[i][0], plCI[i][1], plCI[i][2])
+#     pwCI[i][3] = calculate_entropy(
+#         pwCI[i][0], pwCI[i][1], pwCI[i][2])
+
+
+# Calculate information gain for each attribute
+Info_sl_D = inforD(sl, [slCI[0][3], slCI[1][3], slCI[2][3]])
+Info_sw_D = inforD(sw, [swCI[0][3], swCI[1][3], swCI[2][3]])
+Info_pl_D = inforD(pl, [plCI[0][3], plCI[1][3], plCI[2][3]])
+Info_pw_D = inforD(pw, [pwCI[0][3], pwCI[1][3], pwCI[2][3]])
+
+# Calculate information gain for each attribute
+gain_sl = info_d - Info_sl_D
+gain_sw = info_d - Info_sw_D
+gain_pl = info_d - Info_pl_D
+gain_pw = info_d - Info_pw_D
+
 
 # แสดงผลการทำงานรอบแรก
-"""
-print("sl count is", sl)
+print("sl count is",sl)
 print("pl count is",pl)
-print("swdent count is",sw)
-print("pw rating count is",pw)
-print("Buy computer count is",buy)
+print("sw count is",sw)
+print("pw count is",pw)
+print("Iris count is",classs)
+
 print("sl Info relate to class",slCI)
 print("pl Info relate to class",plCI)
-print("swdent Info relate to class",swCI)
-print("pw rating Info relate to class",pwCI)
+print("sw Info relate to class",swCI)
+print("pw Info relate to class",pwCI)
 
-print("Info(D) is %5.3f" % InD)
-print("Info(sl S(2,3) is %5.3f" % slCI[0][2])
-print("Info(sl 31...40(4,0) is %5.3f" % slCI[1][2])
-print("Info(sl >40 (3,2) is %5.3f" % slCI[2][2])
+print("Info(D) is %5.3f" % info_d)
+print("Info(sl (< mean - SD) is %5.3f" % slCI[0][3])
+print("Info(sl (mean+-2sd) is %5.3f" % slCI[1][3])
+print("Info(sl (> mean + SD) is %5.3f" % slCI[2][3])
 
-print("Info(pl low(1,3) is %5.3f" % plCI[0][2])
-print("Info(pl medium(2,4) is %5.3f" % plCI[1][2])
-print("Info(pl high(2,2) is %5.3f" % plCI[2][2])
+print("Info(sw (< mean - SD) is %5.3f" % swCI[0][3])
+print("Info(sw (mean+-2sd) is %5.3f" % swCI[1][3])
+print("Info(sw (> mean + SD) is %5.3f" % swCI[2][3])
 
-print("Info(swdent No (4,3) is %5.3f" % swCI[0][2])
-print("Info(swdent Yes (1,6) is %5.3f" % swCI[1][2])
+print("Info(pl (< mean - SD) is %5.3f" % plCI[0][3])
+print("Info(pl (mean+-2sd) is %5.3f" % plCI[1][3])
+print("Info(pl (> mean + SD) is %5.3f" % plCI[2][3])
 
-print("Info(pw fair(2,6) is %5.3f" % pwCI[0][2])
-print("Info(pw excellent(3,3) is %5.3f" % pwCI[1][2])
-print("Info sl (D) is %5.3f" % Info_slD)
-print("Info pl (D) is %5.3f" % Info_plD)
-print("Info swdent (D) is %5.3f" % Info_swdentD)
-print("Info pw rating (D) is %5.3f" % Info_pwD)
-"""
+print("Info(pw (< mean - SD) is %5.3f" % pwCI[0][3])
+print("Info(pw (mean+-2sd) is %5.3f" % pwCI[1][3])
+print("Info(pw (> mean + SD) is %5.3f" % pwCI[2][3])
+
+print("Info sl (D) is %5.3f" % Info_sl_D)
+print("Info pl (D) is %5.3f" % Info_pl_D)
+print("Info sw (D) is %5.3f" % Info_sw_D)
+print("Info pw (D) is %5.3f" % Info_pw_D)
+
 print("\n***Gain results of all dataset***")
-gainsl = InD - Info_slD
-print("Gain (sl) is %5.3f" % gainsl)
-gainIn = InD - Info_plD
-print("Gain (pl) is %5.3f" % gainIn)
-gainsw = InD - Info_swdentD
-print("Gain (swdent) is %5.3f" % gainsw)
-gainCre = InD - Info_pwD
-print("Gain (pw rating) is %5.3f" % gainCre)
+gain_sl=info_d-Info_sl_D
+print("Gain (age) is %5.3f"% gain_sl)
+gain_sw=info_d-Info_pl_D
+print("Gain (Income) is %5.3f"% gain_sw)
+gain_pl=info_d-Info_sw_D
+print("Gain (Student) is %5.3f"% gain_pl)
+gain_pw=info_d-Info_pw_D
+print("Gain (Credit rating) is %5.3f"% gain_pw)
 
-# rule of root node
+#rule of root node
 
-Result_All = [gainsl, gainIn, gainsw, gainCre]
-max_gain = max(Result_All)
-pos = np.argmax(Result_All)
+Result_All=[gain_sl, gain_sw, gain_pl, gain_pw]
+max_gain=max(Result_All)
+pos=np.argmax(Result_All)
 print("max gain of attb is %5.3f" % max_gain, "position is", pos)
 
-# วน loop แยก dataset ตาม attb sl
-X2L = []  # ข้อมูลสำหรับสร้าง level 2 ที่ sl S
-X2M = []  # ข้อมูลสำหรับสร้าง level 2 ที่ sl M
-X2R = []  # ข้อมูลสำหรับสร้าง level 2 ที่ sl L
-f1 = open("buycomL2left.txt", "w")
-f2 = open("buycomL2middle.txt", "w")
-f3 = open("buycomL2right.txt", "w")
-
-for i in range(0, 15):
-    if X[i].count("S") == 1:
-        f1.write(str(X[i]))
-
-    elif X[i].count("M") == 1:
-        f2.write(str(X[i]))
-
-    elif X[i].count("L") == 1:
-        f3.write(str(X[i]))
-
-# dataset of layer 2 of dtree generate
-f1 = open("buycomL2left.txt", "r")
-f2 = open("buycomL2middle.txt", "r")
-f3 = open("buycomL2right.txt", "r")
-X2L = f1.readlines()
-X2M = f2.readlines()
-X2R = f3.readlines()
-
-
-# recursive line 14 สำหรับการสร้าง tree ชั้นที่ 2 สำหรับ dataset slS
-
-
-# วน loop เพื่อนับข้อมูล แยกตามรายละเอียด attb และ class
-# data ที่ sl S
-
-
-# calculate information gain of dataset and attb
-# info D,sl,pl,sw,pw
-
-
-# หาค่า gain แบบใช้ฟังก์ชัน
-
-
-# แสดงผลการทำงาน รอบ2 ฝั่งซ้าย
-
-
-# recursive line 14 สำหรับการสร้าง tree ชั้นที่ 2 สำหรับ dataset sl>40
-
-
-# วน loop เพื่อนับข้อมูล แยกตามรายละเอียด attb และ class
-# data ที่ sl >40
-
-
-# calculate information gain of dataset and attb
-# info D,sl,pl,sw,pw
-
-
-# หาค่า gain แบบใช้ฟังก์ชัน
-
-
-# แสดงผลการทำงานรอบสอง ฝั่งขวา
-
-
-# สร้าง tree
-# rule extraction
-# model evaluation
